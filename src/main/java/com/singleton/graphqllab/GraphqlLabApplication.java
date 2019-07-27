c301ab2fbad541380afdf4f3603a33fac61e7342
Anton Prokhorov <betrayer11@gmail.com>
2019-07-27 22:24:43 +0300

    Initial commit

---
+++ src/main/java/com/singleton/graphqllab/GraphqlLabApplication.java
@@ -0,0 +1,13 @@
+package com.singleton.graphqllab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlLabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraphqlLabApplication.class, args);
+  }
+
+}
